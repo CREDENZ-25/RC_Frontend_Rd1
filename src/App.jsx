e3787d4c5b2669aafc9027f5777326932c345a57
vedant-vh <vedanthadap123@gmail.com>
2025-02-14 19:02:39 +0530

    Leaderboard page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
+import Navbar from './Navbar/Navbar.jsx';
+import Layout from './Layout/Layout.jsx';
 
 function App() {
 
   return (
     <>
-      <div className='text-3xl font-bold underline'>
-        <h1>RC Frontend</h1>
+      <div className="main-app h-[100vh] w-[100vw] flex flex-col justify-center items-center  ">
+        <Navbar/>
+      <Layout/>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
-
+import './Navbar.css';
 
 const Navbar = () => {
-
   return (
     <>
-        <h1>Navbar</h1>
-      
+    <div className="Navbar">
+      <div>
+        <h3 className='rc'>RC</h3>
+      </div>
+      <div>
+        <a className="leaderboard" href="">LEADERBOARD</a>
+        <a className="result" href="">RESULT</a>
+
+        {/* Box behind Logout */}
+        <div className="logout-box">
+          <a className="logout" href="">LOGOUT</a>
+        </div>
+      </div>
+    </div>  
     </>
   );
 };
