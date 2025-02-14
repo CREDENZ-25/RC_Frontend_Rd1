2ef67e7d1ea80378fe7a9c059f983cfa7770dc4e
Deepika Sharma <sharmadeepi200@gmail.com>
2025-02-14 10:27:40 +0530

    Create NextPage.jsx

---
+++ Pages/NextPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NextPage.css";
+
+const NextPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the New Page</h1>
+    </div>
+  );
+};
+
+export default NextPage;
